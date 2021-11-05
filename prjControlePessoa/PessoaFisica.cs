9c29694d9c1a3497ed2836482b8882474bd2bebe
Alex5ander <alexsandergutierreez@gmail.com>
2021-11-05 20:34:43 -0300

    Initial commit

--- prjControlePessoa/PessoaFisica.cs
+++ prjControlePessoa/PessoaFisica.cs
@@ -20,6 +20,39 @@ namespace prjControlePessoa
             string[] digitos = CPF.ToCharArray()
             .Select(i => i.ToString()).ToArray();
 
+            int peso = 10;
+            int soma = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                soma += peso * Int16.Parse(digitos[i]); 
+                peso --;
+            }
+
+            int resto = soma % 11;
+            int div1 = resto < 2 ? 0 : 11 - resto;
+
+            if(div1.ToString() != digitos[9]) 
+            {
+                return false;
+            }
+
+            peso = 11;
+            soma = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                soma += peso * Int16.Parse(digitos[i]);
+                peso--;
+            }
+
+            resto = soma % 11;
+            int div2 = resto < 2 ? 0 : 11 - resto;
+
+            if(div2.ToString() != digitos[10]) {
+                return false;
+            }
+
+            Console.WriteLine(div1);
             return true;
         }
     }
